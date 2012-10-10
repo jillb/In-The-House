1632580f2bd8c77c0246ef0d104fd70a367891df
Jill Binder <jill@jillbinder.com>
2012-10-09 19:16:58 -0700

    made 404 page; made posttypes and gave them icons; 404.php, functions.php, posttypes.php, event*.png, production*.png, performer*.png

--- wp-content/themes/in-the-house-festival/functions.php
+++ wp-content/themes/in-the-house-festival/functions.php
@@ -1,3 +1,3 @@
 <?php
-// include_once(ABSPATH . 'wp-content/themes/posttypes.php');
+ include_once(ABSPATH . 'wp-content/themes/posttypes.php');
 ?>
--- wp-content/themes/posttypes.php
+++ wp-content/themes/posttypes.php
@@ -1,25 +1,25 @@
 <?php
 
-// Add new post type for Recipes
-add_action('init', 'cooking_recipes_init');
-function cooking_recipes_init() 
+// Add new post type for Productions
+add_action('init', 'inthehouse_productions_init');
+function inthehouse_productions_init() 
 {
-	$recipe_labels = array(
-		'name' => _x('Recipes', 'post type general name'),
-		'singular_name' => _x('Recipe', 'post type singular name'),
-		'all_items' => __('All Recipes'),
-		'add_new' => _x('Add new recipe', 'recipes'),
-		'add_new_item' => __('Add new recipe'),
-		'edit_item' => __('Edit recipe'),
-		'new_item' => __('New recipe'),
-		'view_item' => __('View recipe'),
-		'search_items' => __('Search in recipes'),
-		'not_found' =>  __('No recipes found'),
-		'not_found_in_trash' => __('No recipes found in trash'), 
+	$production_labels = array(
+		'name' => _x('Productions', 'post type general name'),
+		'singular_name' => _x('Production', 'post type singular name'),
+		'all_items' => __('All Productions'),
+		'add_new' => _x('Add new production', 'productions'),
+		'add_new_item' => __('Add new production'),
+		'edit_item' => __('Edit production'),
+		'new_item' => __('New production'),
+		'view_item' => __('View production'),
+		'search_items' => __('Search in productions'),
+		'not_found' =>  __('No productions found'),
+		'not_found_in_trash' => __('No productions found in trash'), 
 		'parent_item_colon' => ''
 	);
 	$args = array(
-		'labels' => $recipe_labels,
+		'labels' => $production_labels,
 		'public' => true,
 		'publicly_queryable' => true,
 		'show_ui' => true, 
@@ -29,31 +29,31 @@ function cooking_recipes_init()
 		'hierarchical' => false,
 		'menu_position' => 5,
 		'supports' => array('title','editor','author','thumbnail','excerpt','comments','custom-fields'),
-		'has_archive' => 'recipes'
+		'has_archive' => 'productions'
 	); 
-	register_post_type('recipes',$args);
+	register_post_type('productions',$args);
 }
 
-// Add new post type for Photos
-add_action('init', 'cooking_photos_init');
-function cooking_Photos_init() 
+// Add new post type for Events
+add_action('init', 'inthehouse_events_init');
+function inthehouse_events_init() 
 {
-	$photo_labels = array(
-		'name' => _x('Photos', 'post type general name'),
-		'singular_name' => _x('Photo', 'post type singular name'),
-		'all_items' => __('All Photos'),
-		'add_new' => _x('Add new photo', 'photos'),
-		'add_new_item' => __('Add new photo'),
-		'edit_item' => __('Edit photo'),
-		'new_item' => __('New photo'),
-		'view_item' => __('View photo'),
-		'search_items' => __('Search in photos'),
-		'not_found' =>  __('No photos found'),
-		'not_found_in_trash' => __('No photos found in trash'), 
+	$event_labels = array(
+		'name' => _x('Events', 'post type general name'),
+		'singular_name' => _x('Event', 'post type singular name'),
+		'all_items' => __('All Events'),
+		'add_new' => _x('Add new event', 'events'),
+		'add_new_item' => __('Add new event'),
+		'edit_item' => __('Edit event'),
+		'new_item' => __('New event'),
+		'view_item' => __('View event'),
+		'search_items' => __('Search in events'),
+		'not_found' =>  __('No events found'),
+		'not_found_in_trash' => __('No events found in trash'), 
 		'parent_item_colon' => ''
 	);
 	$args = array(
-		'labels' => $photo_labels,
+		'labels' => $event_labels,
 		'public' => true,
 		'publicly_queryable' => true,
 		'show_ui' => true, 
@@ -63,31 +63,31 @@ function cooking_Photos_init()
 		'hierarchical' => false,
 		'menu_position' => 5,
 		'supports' => array('title','editor','author','thumbnail','excerpt','comments','custom-fields'),
-		'has_archive' => 'photos'
+		'has_archive' => 'events'
 	); 
-	register_post_type('photos',$args);
+	register_post_type('events',$args);
 }
 
-// Add new post type for Videos
-add_action('init', 'cooking_videos_init');
-function cooking_videos_init() 
+// Add new post type for Performers
+add_action('init', 'inthehouse_performers_init');
+function inthehouse_performers_init() 
 {
-	$video_labels = array(
-		'name' => _x('Videos', 'post type general name'),
-		'singular_name' => _x('Video', 'post type singular name'),
-		'all_items' => __('All Videos'),
-		'add_new' => _x('Add new video', 'videos'),
-		'add_new_item' => __('Add new video'),
-		'edit_item' => __('Edit video'),
-		'new_item' => __('New video'),
-		'view_item' => __('View video'),
-		'search_items' => __('Search in videos'),
-		'not_found' =>  __('No videos found'),
-		'not_found_in_trash' => __('No videos found in trash'), 
+	$performer_labels = array(
+		'name' => _x('Performers', 'post type general name'),
+		'singular_name' => _x('Performer', 'post type singular name'),
+		'all_items' => __('All Performers'),
+		'add_new' => _x('Add new performer', 'performers'),
+		'add_new_item' => __('Add new performer'),
+		'edit_item' => __('Edit performer'),
+		'new_item' => __('New performer'),
+		'view_item' => __('View performer'),
+		'search_items' => __('Search in performer'),
+		'not_found' =>  __('No performers found'),
+		'not_found_in_trash' => __('No performers found in trash'), 
 		'parent_item_colon' => ''
 	);
 	$args = array(
-		'labels' => $video_labels,
+		'labels' => $performer_labels,
 		'public' => true,
 		'publicly_queryable' => true,
 		'show_ui' => true, 
@@ -97,40 +97,67 @@ function cooking_videos_init()
 		'hierarchical' => false,
 		'menu_position' => 5,
 		'supports' => array('title','editor','author','thumbnail','excerpt','comments','custom-fields'),
-		'has_archive' => 'videos'
+		'has_archive' => 'performers'
 	); 
-	register_post_type('videos',$args);
+	register_post_type('performers',$args);
 }
 
 
 // Add custom taxonomies
-add_action( 'init', 'cooking_create_taxonomies', 0 );
+add_action( 'init', 'inthehouse_create_taxonomies', 0 );
 
-function cooking_create_taxonomies() 
+function inthehouse_create_taxonomies() 
 {
-	// Meal type
+	// Show type
 
-		$meal_labels = array(
-		'name' => _x( 'Meal type', 'taxonomy general name' ),
-		'singular_name' => _x( 'Meal type', 'taxonomy singular name' ),
-		'search_items' =>  __( 'Search in meal type' ),
-		'all_items' => __( 'All meal type' ),
+		$show_labels = array(
+		'name' => _x( 'Show type', 'taxonomy general name' ),
+		'singular_name' => _x( 'Show type', 'taxonomy singular name' ),
+		'search_items' =>  __( 'Search in show type' ),
+		'all_items' => __( 'All show type' ),
 		'most_used_items' => null,
 		'parent_item' => null,
 		'parent_item_colon' => null,
-		'edit_item' => __( 'Edit meal type' ), 
-		'update_item' => __( 'Update meal type' ),
-		'add_new_item' => __( 'Add new meal type' ),
-		'new_item_name' => __( 'New meal type' ),
-		'menu_name' => __( 'Meal type' ),
+		'edit_item' => __( 'Edit show type' ), 
+		'update_item' => __( 'Update show type' ),
+		'add_new_item' => __( 'Add new show type' ),
+		'new_item_name' => __( 'New show type' ),
+		'menu_name' => __( 'Show type' ),
 	);
-	register_taxonomy('meal-type',array('recipes','photos','videos','post'),array(
+	register_taxonomy('show-type',array('productions','events'),array(
 		'hierarchical' => true,
-		'labels' => $meal_labels,
+		'labels' => $show_labels,
 		'show_ui' => true,
 		'query_var' => true,
-		'rewrite' => array('slug' => 'meal-type' )
+		'rewrite' => array('slug' => 'show-type' )
 	));
 }
+
+// Add new Custom Post Type icons
+add_action( 'admin_head', 'show_icons' );
+function show_icons() {
+?>
+	<style type="text/css" media="screen">
+		#menu-posts-productions .wp-menu-image {
+			background: url(<?php bloginfo('url') ?>/wp-content/themes/images/productionsmall.png) no-repeat 6px !important;
+		}
+		.icon32-posts-productions {
+			background: url(<?php bloginfo('url') ?>/wp-content/themes/images/production.png) no-repeat !important;
+		}
+		#menu-posts-events .wp-menu-image {
+			background: url(<?php bloginfo('url') ?>/wp-content/themes/images/eventsmall.png) no-repeat 6px !important;
+		}
+		.icon32-posts-events {
+			background: url(<?php bloginfo('url') ?>/wp-content/themes/images/event.png) no-repeat !important;
+		}
+		#menu-posts-performers .wp-menu-image {
+			background: url(<?php bloginfo('url') ?>/wp-content/themes/images/performersmall.png) no-repeat 6px !important;
+		}
+		.icon32-posts-performers {
+			background: url(<?php bloginfo('url') ?>/wp-content/themes/images/performer.png) no-repeat !important;
+		}
+
+    </style>
+<?php } 
 
 ?>
